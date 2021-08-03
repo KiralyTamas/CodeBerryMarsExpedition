6d3a53c7a1fdd57a752eb1417512f62860d7033d
KiralyTamas <tomaszpest@gmail.com>
2021-08-03 15:46:19 +0200

    próbálkozás ezerrel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+let atlagFogyasztas = document.getElementById("atlagFogyasztas").innerHTML;
+  let foldTavolsag = document.getElementById("foldTavolsag").innerHTML;
+  let tartaly = document.getElementById("tartaly").innerHTML;
+  
 function uzemanyag(atlagFogyasztas, foldTavolsag, tartaly) {
   let szuksegesUzemanyag = atlagFogyasztas / 100 * foldTavolsag;
   let folosUzemanyag = tartaly - szuksegesUzemanyag;
