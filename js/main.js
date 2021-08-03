f03419da0b35414a2f7ad522c9b407c12b1a0b64
KiralyTamas <tomaszpest@gmail.com>
2021-08-03 11:11:17 +0200

    Gombbal működik a számolás

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 function uzemanyag(atlagFogyasztas, foldTavolsag, tartaly) {
   let szuksegesUzemanyag = atlagFogyasztas / 100 * foldTavolsag;
-  let folosUzemanyag= tartaly-szuksegesUzemanyag;
-  console.log(folosUzemanyag);
+  let folosUzemanyag = tartaly - szuksegesUzemanyag;
+  if (document.getElementById("window").innerHTML === "Mars") {
+    document.getElementById("window").innerHTML = folosUzemanyag;
+  }
+  else {
+    document.getElementById("window").innerHTML = "Mars";
+  }
 }
